8582745c08e6739dffb4702cfc3e771b4c28c4ed
Brin29 <breinerstivenparracortes50@gmail.com>
2024-09-15 20:11:09 -0500

    Creation of controllers about Task and User

--- src/main/java/todo/system/Controllers/TasksController.java
+++ src/main/java/todo/system/Controllers/TasksController.java
@@ -1,4 +1,44 @@
 package todo.system.Controllers;
 
+import org.springframework.web.bind.annotation.*;
+import todo.system.Entitys.Task;
+import todo.system.Repository.TaskRepository;
+
+@RestController
 public class TasksController {
+
+    // Inyeccion de dependencias
+    private final TaskRepository repository;
+
+    public TasksController(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping("/tasks")
+    public Task createTask(@RequestBody Task task) {
+        return repository.save(task);
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task getTask(@PathVariable Long id){
+        return repository.findById(id).orElseThrow(() -> new NotFoundTaskException);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task newTask){
+        return repository.findById(id).map(task -> {
+            task.setTitle(newTask.getTitle());
+            task.setDescription(newTask.getDescription());
+            task.setStatus(newTask.getStatus());
+            task.setUser(newTask.getUser());
+
+            return repository.save(task);
+        }).orElseGet(() -> repository.save(newTask));
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public void deleteTask(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
 }
--- src/main/java/todo/system/Controllers/UsersController.java
+++ src/main/java/todo/system/Controllers/UsersController.java
@@ -1,7 +1,54 @@
 package todo.system.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import todo.system.Entitys.User;
+import todo.system.Repository.UserRepository;
+
+import java.util.List;
 
 @RestController
 public class UsersController {
+
+    // Inyeccion de la dependencia
+    private final UserRepository repository;
+
+    public UsersController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping("/users")
+    public  User addUser(@RequestBody User user){
+        return repository.save(user);
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUser(@PathVariable Long id){
+        return repository.findById(id).orElseThrow(() -> new UserNotFoundException);
+    }
+
+    @PutMapping("/users/{id}")
+    public User putUser(@PathVariable Long id,
+    @RequestBody User newUser){
+        return repository.findById(id).map(user -> {
+
+            user.setName(newUser.getName());
+            user.setEmail(newUser.getEmail());
+
+            return repository.save(user);
+        })
+        //En caso de que no lo encuentre lo va a crear
+        .orElseGet(() -> {
+            return repository.save(newUser);
+        });
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return repository.findAll();
+    }
 }
