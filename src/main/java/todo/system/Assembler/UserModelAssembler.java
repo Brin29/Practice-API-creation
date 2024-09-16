9b9e5169f563727114579614c012259d3148246e
Brin29 <breinerstivenparracortes50@gmail.com>
2024-09-15 20:49:45 -0500

    Continuacion para mañana

--- src/main/java/todo/system/Assembler/UserModelAssembler.java
+++ src/main/java/todo/system/Assembler/UserModelAssembler.java
@@ -1,4 +1,27 @@
 package todo.system.Assembler;
 
-public class UserModelAssembler {
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import todo.system.Controllers.TasksController;
+import todo.system.Controllers.UsersController;
+import todo.system.Entitys.User;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+// EntityModel convierte los Usuarios en un entity que contiene los enlaces
+@Component
+public class UserModelAssembler implements RepresentationModelAssembler<User, EntityModel<User>> {
+
+    @Override
+    public EntityModel<User> toModel(User user){
+
+        return EntityModel.of(user,
+                linkTo(methodOn(UsersController.class).one(user.getId())).withSelfRel(),
+                linkTo(methodOn(UsersController.class).all()).withRel("users"),
+                // Comunicacion entre ambas
+                linkTo(methodOn(TasksController.class).allTaskByUser(user.getId())).withRel("tasks"));
+    }
+
 }
